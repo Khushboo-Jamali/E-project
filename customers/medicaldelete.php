3ffa94d3e5528c26a6b19bc24324a795d40518df
khushboo-jamali <jamalikhushboo@gmail.com>
2024-10-03 02:29:47 +0500

    oop class and object practice

--- customers/medicaldelete.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost","root","","rapid_rescue");
-    if(isset($_GET['id'])){
-        $id = $_GET['id'];
-        $del = "DELETE FROM `add_medical_history` WHERE amh_id = '$id'";
-        mysqli_query($conn,$del);
-        header("location:view_medical_history.php");
-    }
-?>
