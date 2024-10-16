135597a4f2479993f98d14b03551363afffbd7ea
khushboo-jamali <jamalikhushboo@gmail.com>
2024-10-16 12:41:31 +0500

    change image i mean logo

--- online_shop/employees/header.php
+++ online_shop/employees/header.php
@@ -57,7 +57,7 @@ if (!isset($_SESSION['userId'])) {
 
     <div class="d-flex align-items-center justify-content-between">
       <a href="index.html" class="logo d-flex align-items-center">
-        <img src="./assets/img/apple-touch-icon.png" alt="">
+        <img src="./images/logo2.png" alt="">
         <span class="d-none d-lg-block">Customer</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
